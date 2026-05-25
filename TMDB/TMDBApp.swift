9274fbd55f7d07849ccafb6c12f758cc45faa5b4
avi651 <kumaravi651@gmail.com>
2026-05-25 17:50:06 +0530

    Initial Commit

---
+++ TMDB/TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBApp.swift
+//  TMDB
+//
+//  Created by AVINASH on 25/05/26.
+//
+
+import SwiftUI
+
+@main
+struct TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
